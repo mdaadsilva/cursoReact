ba53831b27a43b8123d2f2bb91c6383d5b7da0bf
Martin <martindealmeida17@gmail.com>
2025-04-29 18:18:24 -0300

    feat: avançando em props

--- src/components/List.js
+++ src/components/List.js
@@ -4,12 +4,15 @@ function List(){
         <>
             <h1>Minha lista</h1>
             <ul>
-               <Item marca="React"/>
-               <Item marca="Next"/>
-                <Item marca="Vue"/>
+               <Item marca="Ferrari" ano_lancamento={1985}/>
+               <Item marca="Fiat" ano_lancamento={1964}/>
+                <Item marca={"Renault"}/>
+                <Item marca="Chevrolet" ano_lancamento={1999}/>
             </ul>
         </>
     )
 }
+
+
 
 export default List;
