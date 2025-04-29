a5f747659073d27c4618a5f724811d6c0c419f8e
Martin <martindealmeida17@gmail.com>
2025-04-29 15:33:27 -0300

    feat: Entendendo o JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 
 function App() {
+
+  const name = "Lucas";
+
+  const newName = name.toUpperCase();
+
+  function sum(a, b) {
+    return a + b;
+  }
+
+  const url = 'https://via.placeholder.com/500';
+
+
   return (
     <div className="App">
-      <h1>Olá React</h1>
-      <p>Meu primeiro projeto</p>
+      <h2>Alterando Jsx</h2>
+      <p>Olá {newName}, tudo bem?</p>
+      <p>O resultado da soma é: {sum(1, 2)}</p>
+      <img src={url} alt="mInha Imagem" />
     </div>
   );
 }
