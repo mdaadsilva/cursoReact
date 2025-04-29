263a126db09dcb116e635ce01454bb5496d9e3aa
Martin <martindealmeida17@gmail.com>
2025-04-29 18:49:53 -0300

    feat: trabalhando com Eventos no React (onClick, onChange e onSubmit)

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld';
-import SayMyName from './components/SayMyName';
-import Frase from './components/Frase';
-import Pessoa from './components/Pessoa';
-import List from './components/List';
+import Evento from './components/Evento';
+import Form from './components/Form';
 
 function App() {
-
-  const nome = "Lucas"
+  const nome = "Martin";
 
   return (
     <div className="App">
-      <h1>Olá React</h1>
-      <Frase/>
-      <Frase/>
-
-      <SayMyName nome="Martin"/>
-      <SayMyName nome="Marcos"/>
-      <SayMyName nome={nome}/>
-      <Pessoa nome="Martin" idade="20" profissao="Programador" foto="https://via.placeholder.com/150"/>
-      <List />
+      <h1>Testando eventos</h1>
+      <Evento numero={1} />
+      <Evento numero={2} />
+      <Form />
     </div>
-  )
+  );
 }
 
 export default App;
